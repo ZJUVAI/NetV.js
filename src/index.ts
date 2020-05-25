66cf4d4ef371db294928a0a9415e5dae1bb82c16
JackieAnxis <jackieanxis@gmail.com>
2020-05-25 14:39:32 +0800

    add file description

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+/**
+ * @author Jiacheng Pan <jackieanxis@gmail.com>
+ * @description Provide the entrance of NetV.js.
+ * @dependences No dependeces yet.
+ */
+
 const NetV = (() => {
     class Core {}
 
