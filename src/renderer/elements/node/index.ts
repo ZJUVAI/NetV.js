52d75f11b09304c2382ad76cd562e1ad6a37de30
brickmaker <zhaoxiaodong@zju.edu.cn>
2020-06-01 14:14:25 +0800

    feat: 🎸 render node fields

--- src/renderer/elements/node/index.ts
+++ src/renderer/elements/node/index.ts
@@ -4,6 +4,37 @@
  */
 
 class RNode {
+    // program
+    private gl: WebGL2RenderingContext
+    private program: WebGLProgram
+
+    // attributes
+    // TODO: consider export from shader
+    private templateAttr = 0
+    private posAttr = 1
+    private sizeAttr = 2
+    private colorAttr = 3
+
+    // uniforms
+
+    // buffers
+    private templateBuffer: WebGLBuffer
+    private dotPosBuffer: WebGLBuffer
+    private dotSizeBuffer: WebGLBuffer
+    private dotColor: WebGLBuffer
+
+    // arrays
+    // prettier-ignore
+    private templateArr = new Float32Array([
+        -0.5, 0.0, 1.0, 0.0, 0.0,
+        0.0, -0.5, 1.0, 1.0, 0.0,
+        0.0, 0.5, 1.0, 0.0, 1.0,
+        0.5, 0.0, 1.0, 1.0, 1.0
+    ])
+    private posArr: Float32Array
+    private sizeArr: Float32Array
+    private colorArr: Float32Array
+
     constructor(limit: number) {
         
     }
