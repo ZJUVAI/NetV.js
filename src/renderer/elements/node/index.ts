f176ffa603db85d23616dc13c2a534323970aae6
brickmaker <zhaoxiaodong@zju.edu.cn>
2020-06-01 11:38:57 +0800

    feat: 🎸 renderer init structrue

---
+++ src/renderer/elements/node/index.ts
@@ -0,0 +1,10 @@
+/**
+ * @author Xiaodong Zhao <zhaoxiaodong@zju.edu.cn>
+ * @description Node using in Renderer
+ */
+
+class RNode {
+    constructor(limit: number) {
+        
+    }
+}
---
+++ src/renderer/index.ts
@@ -0,0 +1,14 @@
+/**
+ * @description render graph using webgl
+ * @author Xiaodong Zhao <zhaoxiaodong@zju.edu.cn>
+ */
+
+class Renderer {
+    public constructor() {
+        // TODO: init operation
+    }
+
+    public draw() {
+        // TODO: draw elements
+    }
+}
