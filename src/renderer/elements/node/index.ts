e59674e97c2fcddd04532a1ec1b68c4b3cfe95e9
brickmaker <zhaoxiaodong@zju.edu.cn>
2020-06-02 19:15:53 +0800

    feat: 🎸 remove shader.js file, direct import

--- src/renderer/elements/node/index.ts
+++ src/renderer/elements/node/index.ts
@@ -3,7 +3,8 @@
  * @description Node using in Renderer
  */
 
-import { vertShaderStr, fragShaderStr } from './shader'
+import vertShaderStr from './vertex.glsl'
+import fragShaderStr from './fragment.glsl'
 import { createProgram, createArrayBuffer } from '../../utils'
 
 export class RNode {
--- src/renderer/elements/node/shader.js
+++
@@ -1,4 +0,0 @@
-import vertShaderStr from './vertex.glsl'
-import fragShaderStr from './fragment.glsl'
-
-export { vertShaderStr, fragShaderStr }
