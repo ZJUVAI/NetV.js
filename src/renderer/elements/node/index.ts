27bbd4b66e2d38e904d4b303ced486da197ab9e5
brickmaker <zhaoxiaodong@zju.edu.cn>
2020-06-04 11:00:28 +0800

    refactor: 💡 change node set attribute, buffer, array logic

--- src/renderer/elements/node/index.ts
+++ src/renderer/elements/node/index.ts
@@ -8,6 +8,22 @@ import vertShaderStr from './vertex.glsl'
 import fragShaderStr from './fragment.glsl'
 import { createProgram, createArrayBuffer } from '../../utils'
 
+enum NodeAttrKey {
+    Template,
+    Position,
+    Size,
+    Color
+}
+
+type NodeAttributes = {
+    name: string
+    index: number
+    size: number
+    isBuildIn?: boolean
+    array?: Float32Array
+    buffer?: WebGLBuffer
+}[]
+
 export class RNode {
     // program
     private gl: WebGL2RenderingContext
@@ -16,6 +32,7 @@ export class RNode {
     private width: number
     private height: number
     private program: WebGLProgram
+    private attributes: NodeAttributes
 
     // attributes
     // TODO: consider export from shader
@@ -51,48 +68,48 @@ export class RNode {
         this.width = width
         this.height = height
 
-        const attributes = [
+        this.attributes = [
             {
                 name: 'inVertexPos',
-                index: this.templateAttr,
+                index: 0,
                 size: 3,
+                isBuildIn: true
             },
             {
                 name: 'inPos',
-                index: this.posAttr,
-                size: 2,
+                index: 1,
+                size: 2
             },
             {
                 name: 'inSize',
-                index: this.sizeAttr,
-                size: 1,
+                index: 2,
+                size: 1
             },
             {
                 name: 'inColor',
-                index: this.colorAttr,
-                size: 4,
+                index: 3,
+                size: 4
             }
         ]
 
-        this.program = createProgram(this.gl, vertShaderStr, fragShaderStr, attributes)
+        this.program = createProgram(this.gl, vertShaderStr, fragShaderStr, this.attributes)
 
         // init arrays
         // prettier-ignore
-        this.templateArr = new Float32Array([
+        this.attributes[NodeAttrKey.Template].array = new Float32Array([
             -0.5, 0.0, 1.0,
             0.0, -0.5, 1.0,
             0.0, 0.5, 1.0,
             0.5, 0.0, 1.0,
         ])
-        this.posArr = new Float32Array(2 * limit)
-        this.sizeArr = new Float32Array(1 * limit)
-        this.colorArr = new Float32Array(4 * limit)
+        this.attributes.forEach((attr) => {
+            if (!attr.isBuildIn) attr.array = new Float32Array(attr.size * this.limit)
+        })
 
         // init buffers
-        this.templateBuffer = createArrayBuffer(this.gl, this.templateArr)
-        this.posBuffer = createArrayBuffer(this.gl, this.posArr)
-        this.sizeBuffer = createArrayBuffer(this.gl, this.sizeArr)
-        this.colorBuffer = createArrayBuffer(this.gl, this.colorArr)
+        this.attributes.forEach((attr) => {
+            attr.buffer = createArrayBuffer(this.gl, attr.array)
+        })
 
         // init uniforms
         this.gl.useProgram(this.program)
@@ -128,43 +145,30 @@ export class RNode {
     public addData(nodes: Node[]) {
         // set array
         nodes.forEach((node, i) => {
-            this.posArr[2 * (this.count + i)] = node.x
-            this.posArr[2 * (this.count + i) + 1] = node.y
+            // TODO: consider node and render node attribute mapping
+            this.attributes[NodeAttrKey.Position].array[2 * (this.count + i)] = node.x
+            this.attributes[NodeAttrKey.Position].array[2 * (this.count + i) + 1] = node.y
 
-            this.sizeArr[this.count + i] = node.r
+            this.attributes[NodeAttrKey.Size].array[this.count + i] = node.r
 
-            this.colorArr[4 * (this.count + i)] = node.fill.r
-            this.colorArr[4 * (this.count + i) + 1] = node.fill.g
-            this.colorArr[4 * (this.count + i) + 2] = node.fill.b
-            this.colorArr[4 * (this.count + i) + 3] = node.fill.a
+            this.attributes[NodeAttrKey.Color].array[4 * (this.count + i)] = node.fill.r
+            this.attributes[NodeAttrKey.Color].array[4 * (this.count + i) + 1] = node.fill.g
+            this.attributes[NodeAttrKey.Color].array[4 * (this.count + i) + 2] = node.fill.b
+            this.attributes[NodeAttrKey.Color].array[4 * (this.count + i) + 3] = node.fill.a
         })
 
-        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.posBuffer)
-        this.gl.bufferSubData(
-            this.gl.ARRAY_BUFFER,
-            2 * this.count * this.posArr.BYTES_PER_ELEMENT,
-            this.posArr,
-            2 * this.count,
-            2 * nodes.length
-        )
-
-        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.sizeBuffer)
-        this.gl.bufferSubData(
-            this.gl.ARRAY_BUFFER,
-            this.count * this.posArr.BYTES_PER_ELEMENT,
-            this.sizeArr,
-            this.count,
-            nodes.length
-        )
-
-        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.colorBuffer)
-        this.gl.bufferSubData(
-            this.gl.ARRAY_BUFFER,
-            4 * this.count * this.posArr.BYTES_PER_ELEMENT,
-            this.colorArr,
-            4 * this.count,
-            4 * nodes.length
-        )
+        this.attributes.forEach((attr) => {
+            if (!attr.isBuildIn) {
+                this.gl.bindBuffer(this.gl.ARRAY_BUFFER, attr.buffer)
+                this.gl.bufferSubData(
+                    this.gl.ARRAY_BUFFER,
+                    attr.size * this.count * attr.array.BYTES_PER_ELEMENT,
+                    attr.array,
+                    attr.size * this.count,
+                    attr.size * nodes.length
+                )
+            }
+        })
 
         this.count += nodes.length
     }
@@ -176,54 +180,22 @@ export class RNode {
         this.gl.clear(this.gl.COLOR_BUFFER_BIT)
         if (this.count > 0) {
             this.gl.useProgram(this.program)
+            this.attributes.forEach((attr) => {
+                this.gl.enableVertexAttribArray(attr.index)
+            })
 
-            this.gl.enableVertexAttribArray(this.templateAttr)
-            this.gl.enableVertexAttribArray(this.posAttr)
-            this.gl.enableVertexAttribArray(this.sizeAttr)
-            this.gl.enableVertexAttribArray(this.colorAttr)
-
-            this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.templateBuffer)
-            this.gl.vertexAttribPointer(
-                this.templateAttr,
-                3,
-                this.gl.FLOAT,
-                false,
-                3 * this.templateArr.BYTES_PER_ELEMENT,
-                0
-            )
-
-            this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.posBuffer)
-            this.gl.vertexAttribPointer(
-                this.posAttr,
-                2,
-                this.gl.FLOAT,
-                false,
-                2 * this.posArr.BYTES_PER_ELEMENT,
-                0
-            )
-            this.gl.vertexAttribDivisor(this.posAttr, 1) // TODO: attribDivisor's usage
-
-            this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.sizeBuffer)
-            this.gl.vertexAttribPointer(
-                this.sizeAttr,
-                1,
-                this.gl.FLOAT,
-                false,
-                1 * this.sizeArr.BYTES_PER_ELEMENT,
-                0
-            )
-            this.gl.vertexAttribDivisor(this.sizeAttr, 1)
-
-            this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.colorBuffer)
-            this.gl.vertexAttribPointer(
-                this.colorAttr,
-                4,
-                this.gl.FLOAT,
-                false,
-                4 * this.colorArr.BYTES_PER_ELEMENT,
-                0
-            )
-            this.gl.vertexAttribDivisor(this.colorAttr, 1)
+            this.attributes.forEach((attr, i) => {
+                this.gl.bindBuffer(this.gl.ARRAY_BUFFER, attr.buffer)
+                this.gl.vertexAttribPointer(
+                    attr.index,
+                    attr.size,
+                    this.gl.FLOAT,
+                    false,
+                    attr.size * attr.array.BYTES_PER_ELEMENT,
+                    0
+                )
+                if (!attr.isBuildIn) this.gl.vertexAttribDivisor(attr.index, 1)
+            })
         }
 
         this.gl.drawArraysInstanced(this.gl.TRIANGLE_STRIP, 0, 4, this.count)
