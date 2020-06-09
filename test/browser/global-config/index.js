dae8430cca672ffb29a1a0b41ea9788f16535e0b
brickmaker <zhaoxiaodong@zju.edu.cn>
2020-06-09 17:04:33 +0800

    test: 💍 global config set

--- test/browser/global-config/index.js
+++ test/browser/global-config/index.js
@@ -4,23 +4,35 @@
  *
  * expected: 4 square draw on screen
  */
-const netv = new NetV(document.getElementById('main'))
+const configs = {
+    node: {
+        r: 10,
+        fill: { r: 1, g: 0, b: 0, a: 0.5 }
+    },
+    container: {
+        width: 1000,
+        height: 1000
+    },
+    nodeLimit: 1000,
+    backgroundColor: { r: 0.3, g: 0.3, b: 0.3, a: 0.3 }
+}
+const netv = new NetV(document.getElementById('main'), configs)
 const testData = {
     nodes: [
         {
             id: '0',
             x: 0,
-            y: 0,
+            y: 0
         },
         {
             id: '1',
             x: 100,
-            y: 500,
+            y: 500
         },
         {
             id: '2',
             x: 400,
-            y: 400,
+            y: 400
         },
         {
             id: '3',
@@ -48,7 +60,7 @@ const testData = {
         },
         {
             source: '0',
-            target: '2',
+            target: '2'
         },
         {
             source: '1',
@@ -59,7 +71,7 @@ const testData = {
                 b: 1,
                 a: 1
             }
-        },
+        }
     ]
 }
 
