be8e766ccbcedf80cd2930f470cb9ac6b5997bdf
brickmaker <zhaoxiaodong@zju.edu.cn>
2020-06-10 14:40:33 +0800

    test: 💍 load dataset and transform graph position

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -8,8 +8,9 @@ import { NodeLinkData } from 'src/interfaces'
 /**
  * given a graph data with position, return a copy of graph, with position transformed to center of given size
  * @param graph node link graph data
- * @param width graph max width
- * @param height graph max height
+ * @param size graph size (max(width, height))
+ * @param centerX x pos of graph center
+ * @param centerY y pos of graph center
  */
 export function transformGraphPosition(
     graph: NodeLinkData,
