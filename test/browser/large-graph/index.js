b2ca00d3210bead739dc09c91df046f35579a984
brickmaker <zhaoxiaodong@zju.edu.cn>
2020-06-17 17:31:20 +0800

    test: 💍 update large graph demo's style

--- test/browser/large-graph/index.js
+++ test/browser/large-graph/index.js
@@ -5,7 +5,12 @@
 const netv = new NetV({
     container: document.getElementById('main'),
     nodeLimit: 1e5,
-    linkLimit: 1e7
+    linkLimit: 1e7,
+    node: {
+        fill: { r: 0.2, g: 0.6, b: 0.2, a: 0.5 },
+        strokeColor: { r: 0.9, g: 0.9, b: 0.9, a: 0.2 },
+        strokeWidth: 1
+    }
 })
 
 fetch('../data/fin512.json')
