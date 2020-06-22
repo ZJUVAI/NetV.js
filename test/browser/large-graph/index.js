0e80e73262cec5b25186504b2f7da54f4004fa0b
brickmaker <zhaoxiaodong@zju.edu.cn>
2020-06-22 18:48:59 +0800

    test: 💍 add more data and changed large graph style

--- test/browser/large-graph/index.js
+++ test/browser/large-graph/index.js
@@ -4,19 +4,24 @@
  */
 const netv = new NetV({
     container: document.getElementById('main'),
+    width: 1000,
+    height: 1000,
     nodeLimit: 1e5,
     linkLimit: 1e7,
     node: {
         fill: { r: 0.2, g: 0.6, b: 0.2, a: 0.5 },
+        r: 1,
         strokeColor: { r: 0.9, g: 0.9, b: 0.9, a: 0.2 },
         strokeWidth: 1
     }
 })
 
-fetch('../data/fin512.json')
+fetch('../data/bcsstk31.txt.json')
+// fetch('../data/3elt.txt.json')
+// fetch('../data/fin512.json')
     .then((res) => res.json())
     .then((json) => {
-        const data = netv.Utils.transformGraphPosition(json, 600, 400, 300)
+        const data = netv.Utils.transformGraphPosition(json, 1000, 500, 500)
         netv.data(data)
         netv.draw()
     })
