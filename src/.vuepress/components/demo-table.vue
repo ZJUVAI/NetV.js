4bcbac138a9404338e9c12bd738730c59110a0a4
JackieAnxis <jackieanxis@gmail.com>
2020-09-23 17:38:33 +0800

    change netv to NetV

--- src/.vuepress/components/demo-table.vue
+++ src/.vuepress/components/demo-table.vue
@@ -1,66 +1,64 @@
 <script>
-const id = 'demo-table'
+const id = "demo-table";
 export default {
-	name: id,
-	data() {
-		return {
-			exampleArr: [
-				'basic',
-				'build-in-dataset',
-				'configs',
-				'external-layout',
-				'interaction',
-				'large-graph',
-            ],
-            DisArr:[
-                'Basic Usage'
-            ]
-		}
-	},
-}
+  name: id,
+  data() {
+    return {
+      exampleArr: [
+        "basic",
+        "build-in-dataset",
+        "configs",
+        "external-layout",
+        "interaction",
+        "large-graph",
+      ],
+      DisArr: ["Basic Usage"],
+    };
+  },
+};
 </script>
 <template>
-	<div>
-		<a-row
-			:gutter="[
-				{ xs: 8, sm: 16, md: 24, lg: 32 },
-				{ xs: 8, sm: 16, md: 24, lg: 32 },
-			]"
-		>
-			<a-col :span="8" v-for="example in exampleArr" :key="example">
-				<a-card hoverable>
-					<img
-						slot="cover"
-						alt="example"
-						v-if="example != 'external-layout'"
-						:src="`/netv.js/${example}.jpg`"
-					/>
-					<img
-						slot="cover"
-						alt="example"
-						v-else
-						:src="`/netv.js/${example}.gif`"
-					/>
-					<template slot="actions" class="ant-card-actions">
-						<a-button
-							type="primary"
-							:href="`https://codesandbox.io/s/github/ZJUVAG/testDemo/tree/master/examples/${example}?file=/index.html`"
-                            :style="{backgroundColor:'#c2185b',borderColor:'#c2185b'}"
-						>
-							Try it!
-						</a-button>
-					</template>
-					<a-card-meta
-                        <!-- v-bind:style="{textTransform: capitalize}" -->
-						:title="example"
-					>
-						<!-- <a-avatar
+  <div>
+    <a-row
+      :gutter="[
+        { xs: 8, sm: 16, md: 24, lg: 32 },
+        { xs: 8, sm: 16, md: 24, lg: 32 },
+      ]"
+    >
+      <a-col :span="8" v-for="example in exampleArr" :key="example">
+        <a-card hoverable>
+          <img
+            slot="cover"
+            alt="example"
+            v-if="example != 'external-layout'"
+            :src="`/NetV.js/${example}.jpg`"
+          />
+          <img
+            slot="cover"
+            alt="example"
+            v-else
+            :src="`/NetV.js/${example}.gif`"
+          />
+          <template slot="actions" class="ant-card-actions">
+            <a-button
+              type="primary"
+              :href="
+                `https://codesandbox.io/s/github/ZJUVAG/testDemo/tree/master/examples/${example}?file=/index.html`
+              "
+              :style="{ backgroundColor: '#c2185b', borderColor: '#c2185b' }"
+            >
+              Try it!
+            </a-button>
+          </template>
+          <a-card-meta <!-- v-bind:style="{ textTransform: capitalize }" -->
+            :title="example" >
+            <!-- <a-avatar
 				slot="avatar"
 				src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
 			/> -->
-					</a-card-meta>
-				</a-card>
-			</a-col>
-		</a-row>
-	</div>
+          </a-card-meta>
+        </a-card>
+      </a-col>
+    </a-row>
+  </div>
 </template>
