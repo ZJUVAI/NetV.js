66c2050e5e97af3e1ab45b3d0099a160c9e5dbd0
brickmaker <zhaoxiaodong@zju.edu.cn>
2021-01-05 17:53:15 +0800

    'DeployedAutomatically'

---
+++ src/.vuepress/components/background-gif.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="background-container">
+        <img id="background" :src="$withBase('/background.gif')" alt="background" />
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped lang='scss'>
+#background-container {
+    $height: 800px;
+    $width: 2200px;
+    $ratio: 0.75;
+    position: absolute;
+    overflow: hidden;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: $height * $ratio;
+
+    #background {
+        height: $height * $ratio;
+        width: $width * $ratio;
+        max-height: $height * $ratio;
+        max-width: $width * $ratio;
+        position: absolute;
+        top: 0;
+        left: 50%;
+        transform: translate(-50%);
+    }
+}
+</style>
